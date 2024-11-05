c0a52d6ec4d44936ea2aec57060f0b93e39bfec1
Xutao Gao <taogeeee@gmail.com>
2024-11-05 14:30:38 -0500

    Add ID field to user response

--- Dtos/Auth/UserResponse.cs
+++ Dtos/Auth/UserResponse.cs
@@ -3,6 +3,8 @@ namespace Pantrify.API.Dtos
 {
 	public class UserResponse
 	{
+		public int Id { get; set; }
+
 		public string Email { get; set; } = null!;
 
 		public string FirstName { get; set; } = null!;
